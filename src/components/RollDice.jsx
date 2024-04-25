24def6e0e83c6ac8280cad7fa6354bb183405ed6
piyushDSM <piyushchaubeydsm@gmail.com>
2024-04-25 07:28:21 +0530

    Update

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,36 +1,31 @@
+import styled from "styled-components";
 
-import styled from "styled-components"
-
-function RollDice( {roledice, currentDice}) {
-
-  
+function RollDice({ roledice, currentDice }) {
   return (
     <DiceContainer>
-       <div className="dice" 
-       onClick= {roledice}>
+      <div className="dice" onClick={roledice}>
         <img src={`/dice/dice_${currentDice}.png`} alt="dice" />
-       </div>
-       <p>Click on DICE to roll</p>
+      </div>
+      <p>Click on DICE to roll</p>
     </DiceContainer>
-  )
+  );
 }
 
-export default RollDice
+export default RollDice;
 
-const DiceContainer = styled.div `
+const DiceContainer = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  p {
+    font-size: 25px;
+    font-weight: 400;
+    margin-top: 10px;
+  }
 
-    p{
-     font-size: 25px;
-     font-weight: 400;
-     margin-top: 10px;
-    }
-
-    .dice {
+  .dice {
     cursor: pointer;
-}
-`
+  }
+`;
