7ff916adf035066836838b0878952d24e75c7065
piyushDSM <piyushchaubeydsm@gmail.com>
2024-04-25 07:27:42 +0530

    Format

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,68 +1,68 @@
-
-import styled from "styled-components"
+import styled from "styled-components";
 
 function NumberSelector({
-  setError,error,selectedNumber, setSelectedNumber}) {
- const arrNumber = [1,2,3,4,5,6];
- 
-const numberHandler = (value) => {
-  setSelectedNumber(value)
-  setError("")
-}
+  setError,
+  error,
+  selectedNumber,
+  setSelectedNumber,
+}) {
+  const arrNumber = [1, 2, 3, 4, 5, 6];
+
+  const numberHandler = (value) => {
+    setSelectedNumber(value);
+    setError("");
+  };
   return (
     <MainContainer>
       <p className="error">{error}</p>
       <div className="NumberBox">
-      {
-        arrNumber.map( (value, i) => (
-         <Box 
-         isSelected = {value == selectedNumber}
-         key={i}
-         onClick={() => numberHandler(value)}>
-        {value}
-        </Box>
+        {arrNumber.map((value, i) => (
+          <Box
+            isSelected={value == selectedNumber}
+            key={i}
+            onClick={() => numberHandler(value)}
+          >
+            {value}
+          </Box>
         ))}
       </div>
       <p>Select Number👆</p>
     </MainContainer>
-  )
+  );
 }
 
-export default NumberSelector
+export default NumberSelector;
 
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
 
-const MainContainer = styled.div `
-
-display: flex;
-flex-direction: column;
-align-items: end;
-
-    .NumberBox{
-        display: flex;
-        gap: 24px;
-    }
-    p{
-        font-size: 24px;
-        font-weight: 600;
-        line-height: 50px;
-    }
-    .error{
-      color: red;
-    }
-`
-
-const Box = styled.div `
-
+  .NumberBox {
     display: flex;
-    background-color: ${(props) => (props.isSelected ? "black" : "white")};
-    color: ${(props) => (props.isSelected ? "white" : "black")};
-    height: 72px;
-    width: 72px;
-    border: 1px solid black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 25px;
+    gap: 24px;
+  }
+  p {
+    font-size: 24px;
     font-weight: 600;
-    cursor: pointer;
-`
+    line-height: 50px;
+  }
+  .error {
+    color: red;
+  }
+`;
+
+const Box = styled.div`
+  display: flex;
+  background-color: ${(props) => (props.isSelected ? "black" : "white")};
+  color: ${(props) => (props.isSelected ? "white" : "black")};
+  height: 72px;
+  width: 72px;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: 600;
+  cursor: pointer;
+`;
